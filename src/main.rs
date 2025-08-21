521489619ff79907249c425393a5c102b51656b5
K9Developer <ilai.keinan@gmail.com>
2025-08-22 00:28:16 +0300

    add: a bunch of todos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use cubicle::constants::versions::{Version, VersionManager};
+use cubicle::constants::versions::{VersionManager};
 use cubicle::models::other::position::Position;
 use cubicle::models::world::world::{World, WorldType};
 use std::time::Instant;
 
 // TODO: Finish all todos before doing more versions!
+
+/*
+TODO: Have a WorldContentManager that will be in charge of the API of the content of the world.
+TODO: For example: have set_block for chunk, dimension, and the WorldContentManager - then they'll each call each other after a bit of position tweaking.
+TODO: have WorldMetadata like WorldContentManager
+TODO: Have a Properties struct so it could be used in entities, blocks, items, etc. It should include the get and set props with path
+TODO: Make the get and set props macros (or use macros) so they just make code at the end without the whole looping and everything - itll be faster.
+*/
 
 fn main() {
     // let v = Version::new("1.20.1", WorldType::SINGLEPLAYER);
@@ -18,7 +26,6 @@ fn main() {
     w.register_regions();
     let start = Instant::now();
     w.load_region(Position::new("overworld", -1f32, 0f32, -1f32));
-
     let duration = start.elapsed();
     println!("Elapsed: {:?}", duration);
     let b = w.get_block_at_position(Position::new("overworld", -504f32, 62f32, -504f32));
